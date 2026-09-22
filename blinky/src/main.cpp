6c7803807973f86ff471d2620ace72e39724d794
Alexandre Schmid <alexandre.schmid@edu.hefr.ch>
2026-09-22 14:57:11 +0200

    ADDED clang-format configuration and format blinky

--- blinky/src/main.cpp
+++ blinky/src/main.cpp
@@ -1,10 +1,26 @@
+/**
+******************************************************************************
+* @file        : main.cpp
+* @brief       : Blinky Application
+* @author      : Alexandre Schmid <alexandre.schmid@master.hes-so.ch>
+* @date        : 22. September 2026
+******************************************************************************
+* @copyright   : Copyright (c) 2026
+*                HES-SO Master
+* @attention   : SPDX-License-Identifier: MIT OR Apache-2.0
+******************************************************************************
+* @details
+* Making LED1 on the board toggle between on and off repeatedly
+******************************************************************************
+*/
+
 // stl
 #include <chrono>
 
 // zpp_lib
-#include "zpp_include/thread.hpp"
-#include "zpp_include/this_thread.hpp"
 #include "zpp_include/digital_out.hpp"
+#include "zpp_include/this_thread.hpp"
+#include "zpp_include/thread.hpp"
 #include "zpp_include/zpp_log.hpp"
 
 ZPP_LOG_MODULE_REGISTER(blinky, CONFIG_APP_LOG_LEVEL);
@@ -24,22 +40,17 @@ int main(void) {
   ZPP_LOG_DBG("Running on board %s", CONFIG_BOARD_TARGET);
   ZPP_LOG_DBG("Starting thread");
 
-  zpp_lib::Thread thread(
-      zpp_lib::PreemptableThreadPriority::PriorityNormal,
-      "BlinkyThread"
-  );
+  zpp_lib::Thread thread(zpp_lib::PreemptableThreadPriority::PriorityNormal, "BlinkyThread");
 
   auto res = thread.start(blink);
   if (!res) {
-    ZPP_LOG_ERR("Could not start thread (%d)",
-                static_cast<int>(res.error()));
+    ZPP_LOG_ERR("Could not start thread (%d)", static_cast<int>(res.error()));
     return -1;
   }
 
   res = thread.join();
   if (!res) {
-    ZPP_LOG_ERR("Could not terminate thread (%d)",
-                static_cast<int>(res.error()));
+    ZPP_LOG_ERR("Could not terminate thread (%d)", static_cast<int>(res.error()));
     return -1;
   }
 
